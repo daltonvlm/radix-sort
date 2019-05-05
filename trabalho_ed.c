99e2eec7194a8b02748f23312d921842c35fdac8
Dalton Martins <daltonvlm@gmail.com>
2019-05-05 02:49:39 -0300

    Corrige formatacao do codigo

--- trabalho_ed.c
+++ trabalho_ed.c
@@ -2,14 +2,16 @@
  * Trabalho de Estruturas de Dados 
  *
  * Implementar um programa que recebe como entrada inteiros n e d e gera
- * aleatoriamente uma lista com n elementos com exatamente d dígitos (o dígito
- * mais significativo pode ser 0). O programa deve listar os n elementos
- * gerados e, a seguir, quando solicitado, executar a ordenação por
- * distribuição. O programa deve imprimir, a cada iteração, o estado corrente
- * de b filas, bem como o estado corrente da lista de elementos a ser ordenada.
+ * aleatoriamente uma lista com n elementos com exatamente d dígitos (o 
+ * dígito mais significativo pode ser 0). O programa deve listar os n 
+ * elementos gerados e, a seguir, quando solicitado, executar a ordenação 
+ * por distribuição. O programa deve imprimir, a cada iteração, o estado 
+ * corrente de b filas, bem como o estado corrente da lista de elementos a 
+ * ser ordenada.
  *
  * Considere b = 10.
- * As impressões do programa devem seguir o modelo do livro-texto (pág. 35).
+ * As impressões do programa devem seguir o modelo do livro-texto (pág. 
+ * 35).
  */
 
 #include <stdio.h>
@@ -36,8 +38,8 @@ void radix_sort(Numero *fila[], char **numeros, size_t n, size_t d);
 int main(void)
 {
 	int n, d;
-	char **numeros;			/* ponteiro para sequência de números em formato 
-							   de string */
+	char **numeros;			/* ponteiro para sequência de números em 
+							   formato de string */
 	Numero *fila[BASE];
 
 	/* Define valor aleatório para rand() */
